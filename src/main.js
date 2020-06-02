833ef0e171a5d4099d149dd370dada6e10d2ce51
Alain Kassarjian <alain.kassarjian@gmail.com>
2020-06-02 16:19:46 -0700

    Added creative tilt comments

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
+//Creative Tilt:
+//Creative choices that I found notable were the decreasing
+//time, adding the task of collecting items while dodging obstacles,
+//and all of the art, sound, and music were created by me.
+//
+//Technical Tilt:
+//I wanted to move beyond basic fonts for this project,
+//so I spent a notable amount of time importing and formatting
+//google fonts for the game. This way I can gain access to
+//many more fonts for future projects.
+
 let config =
 {
     type: Phaser.CANVAS,
